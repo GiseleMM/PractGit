eb796562fc8b8597a5cefce19d8ffcc2a40e2ce6
gisele <giselemedina01@gmail.com>
2022-03-05 15:38:02 -0300

    elimine tp por que eclipse no me dejaba importarlo y le cambie nombre

---
+++ tp3_windowsCopia/Employee.c
@@ -0,0 +1,33 @@
+#include "Employee.h"
+
+
+Employee* employee_new()
+{
+	Employee* pElement=NULL;
+	pElement=(Employee*)malloc(sizeof(Employee));
+	if(pElement!=NULL)
+	{
+		pElement->id=0;
+		strcpy(pElement->nombre," ");
+		pElement->horasTrabajas=0;
+		pElement->sueldo=0;
+	}
+	return pElement;
+}
+/*
+Employee* employee_newParametros(char* idStr,char* nombreStr,char* horasTrabajadasStr);
+void employee_delete();
+
+int employee_setId(Employee* this,int id);
+int employee_getId(Employee* this,int* id);
+
+int employee_setNombre(Employee* this,char* nombre);
+int employee_getNombre(Employee* this,char* nombre);
+
+int employee_setHorasTrabajadas(Employee* this,int horasTrabajadas);
+int employee_getHorasTrabajadas(Employee* this,int* horasTrabajadas);
+
+int employee_setSueldo(Employee* this,int sueldo);
+int employee_getSueldo(Employee* this,int* sueldo);
+
+*/
