b60718fa190235c07d8a476372e5f9aab1d31d29
gisele <giselemedina01@gmail.com>
2022-03-09 20:30:41 -0300

    Cree para rama parser , empezando LoadFromText

--- tp3_windowsCopia/parser.c
+++ tp3_windowsCopia/parser.c
@@ -9,8 +9,18 @@
  */
 int parser_EmployeeFromText(FILE* pFile , LinkedList* pArrayListEmployee)
 {
-
-    return 1;
+int todoOk=0;
+char buffer[4][30];
+//int contador=0;
+//int cant;
+if(pFile!=NULL && pArrayListEmployee!=NULL)
+{
+	todoOk=1;
+	/*lectura fanstasma del encabezado*/
+	fscanf(pFile,"%[^,],%[^,],%[^,],%[^\n]\n",buffer[0],buffer[1],buffer[2],buffer[3]);
+	printf("%s-%s-%s-%s-",buffer[0],buffer[1],buffer[2],buffer[3]);
+}
+    return todoOk;
 }
 
 /** \brief Parsea los datos los datos de los empleados desde el archivo data.csv (modo binario).
